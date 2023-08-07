1b57c9aa8576688bf8caec3c3520d0c607db4b4b
rvineet <rvineet663@gmail.com>
2023-08-07 23:25:22 +0530

    ep 8 in progress

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,54 @@
+import React from "react";
 import User from "./User";
 import UserClass from "./UserClass";
-const About = () => {
-    return(
-        <div>
-            <h1>About us</h1>
-            <h1>This is Namastey React by ME!</h1>
-            {/* <User name="Vineet (Functional)"/> */}
-            <UserClass name="Vineet (Class)" location="Gurgaon Class"/>
+// const About = () => {
+//     return(
+//         <div>
+//             <h1>About us</h1>
+//             <h1>This is Namastey React by ME!</h1>
+//             {/* <User name="Vineet (Functional)"/> */}
+//             <UserClass name="Vineet (Class)" location="Gurgaon Class"/>
 
-        </div>
-    )
+//         </div>
+//     )
+// }
+
+class About extends React.Component {
+    constructor(props){
+        super(props)
+        // console.log("parent constructor is called")
+    }
+    componentDidMount(){
+        // console.log("parent componentDidMount is called")
+      }
+  render() { 
+    // console.log("parent render is called")
+    return (
+      <div>
+        <h1>About us</h1>
+        <h1>This is Namastey React by ME!</h1>
+        {/* <User name="Vineet (Functional)"/> */}
+        <UserClass name="first" location="Gurgaon Class" />
+        {/* <UserClass name="second" location="Gurgaon Class" /> */}
+      </div>
+    );
+  }
 }
 export default About;
+
+/* 
+-parent constructor is called
+-parent render is called
+
+   -firstchild constructor is called
+   -firstchild render is called
+   -secondchild constructor is called
+   -secondchild render is called
+
+
+   <***DOM Updated in SINGLE BATCH***> (COMMIT PHASE)
+
+   -firstchild componentDidMount is called
+   -secondchild componentDidMount is called
+-parent componentDidMount is called
+*/
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,28 +1,76 @@
 import React from "react";
 
+// https://projects.wojtekmaj.pl/react-lifecycle-methods-diagram/
+// svery important to learn component lifecycles
+
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      count: 0,
-      count2:2
+      userInfo: {
+        name: "dummy",
+        location: "default",
+      },
     };
+    console.log("child constructor is called");
+  }
+  async componentDidMount() {
+    console.log("child componentDidMount is called");
+    const data = await fetch("https://api.github.com/users/rvineet");
+    const json = await data.json();
+    this.setState({
+      userInfo: json,
+    });
+    console.log(json, "===json==");
+  }
+
+  componentDidUpdate() {
+    console.log("component did update is called");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount is called");
   }
   render() {
+    console.log("child render is called");
+    const { name, location, avatar_url } = this.state.userInfo;
     return (
       <div className="user-card">
-        <h1>Count = {this.state.count}</h1>
+        {/* <h1>Count = {this.state.count}</h1>
         <button onClick={() => {
             this.setState({
                 count:this.state.count + 1
             })
-        }}>Count Increase</button>
+        {/* }}>Count Increase</button> */}
         {/* <h1>Count2 = {this.state.count2}</h1> */}
-        <h2>Name:{this.props.name}</h2>
-        <h3>Location: {this.props.location}</h3>
+        <img src={avatar_url}></img>
+        <h2>Name:{name}</h2>
+        <h3>Location: {location}</h3>
         <h4>Contact: rvineet663</h4>
       </div>
     );
   }
 }
 export default UserClass;
+
+/****
+ *
+ * --- MOUNTING ----
+ *
+ * Constructor (dummy)
+ * Render (dummy)
+ *      <HTML Dummy >
+ * Component Did MOunt
+ *      <API Call>
+ *      <this.setState> -> State variable is updated
+ *
+ * ---- UPDATE
+ *
+ *      render(APi data)
+ *      <HTML (new API data>)
+ *      ccomponentDid Update
+ *
+ *
+ *
+ *
+ */
